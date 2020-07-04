b71b9c3be5d19fcfa9d05fe64f811afde62a37a5
scamp <scampovich@gmail.com>
2020-07-05 00:04:13 +0300

    pylint check

--- pynb.py
+++ pynb.py
@@ -1,11 +1,14 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+""" simple task for mass nb routine """
 
-import pynetbox
 import sys
+import pynetbox
 
 def main():
+    """ all in one """
+
     print(f"arg: {sys.argv} ... ")
-    if len(sys.argv) < 3: 
+    if len(sys.argv) < 3:
         print(f"sorry, {sys.argv[0]} dorsn't have enought tips")
         sys.exit(-1)
     nb_url = sys.argv[1]
@@ -13,15 +16,15 @@ def main():
     #all_prefixes = nbox.ipam.prefixes.all()
     try:
         get_devs = nbox.dcim.devices.filter('gpn')
-    except pynetbox.core.query.RequestError as e:
-        print(f"wooops bad bad - {e}")
+    except pynetbox.core.query.RequestError as err:
+        print(f"wooops bad bad - {err}")
         sys.exit()
 
     moned = []
     not_moned = []
     indicies = []
 
-    for key,val in enumerate(get_devs):
+    for key, val in enumerate(get_devs):
         if "mon" in val.tags:
             #print("already monitored:")
             print(f"monitored - name: {val.name} / tags: {val.tags} / index = {key}")
@@ -35,15 +38,15 @@ def main():
 
     #print(f"moned: {moned}")
     #print(f"not moned: {not_moned}")
-    x = input("save? (Y/N): ")
-    if x == "Y": 
+
+    if input("save? (Y/N): ") == "Y":
         print("YED!")
         for _ in indicies:
             print(f"{_}: name: {get_devs[_].name} / tags: {get_devs[_].tags}")
-            y = input("\n\n save save? (Y/N)")
-            if y == "Y": get_devs[_].save()
+            if input("save? (Y/N): ") == "Y":
+                get_devs[_].save()
     else: print("No...")
 
 if __name__ == '__main__':
     main()
-    exit()
+    sys.exit()
